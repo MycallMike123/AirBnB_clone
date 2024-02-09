32b9f6335a8201a866af7a4f8fc51ce8de0a84e7
Michael Mwangi <michaelmichire2016@gmail.com>
2024-02-09 09:46:32 -0800

    A class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -29,26 +29,22 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """
-        String representation when instance is printed
-        """
+        """String representation of BaseModel instance"""
+
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """
-        Save updates to an instance
-        """
+        """Update the public instance attr updated_at with the datetime"""
         self.__dict__.update({'updated_at': datetime.now()})
         from .__init__ import storage
         storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary representation of an instance
-        """
-        disdict = dict(self.__dict__)
-        disdict.update({'__class__': type(self).__name__,
+        """Returns a dictionary containing all keys/values of instances"""
+
+        new_dict = dict(self.__dict__)
+        new_dict.update({'__class__': type(self).__name__,
                         'updated_at': self.updated_at.isoformat(),
-                        'id': self.id,
-                        'created_at': self.created_at.isoformat()})
-        return disdict
+                         'id': self.id,
+                         'created_at': self.created_at.isoformat()})
+        return new_dict
