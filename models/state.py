20b64b5a66a87342f6524074804a346ed04414dc
Michael Mwangi <michaelmichire2016@gmail.com>
2024-02-11 05:48:47 -0800

    A script that sets permission

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module that Defines the state model"""
+
+from .base_model import BaseModel
+
+
+class State(BaseModel):
+    """A class with the Blueprint for State objects"""
+
+    name = ""
