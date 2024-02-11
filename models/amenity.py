0a25495e523c2ae3854d31063f52ece868e62de7
Michael Mwangi <michaelmichire2016@gmail.com>
2024-02-11 05:56:44 -0800

    A script that sets permission

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module that Defines the state model"""
+
+from .base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """A class that sets the blueprint for Amenity objects"""
+
+    name = ""
